093728fded7e8bc203404e3ff73bafcff4d31969
zhufeng <zhufeng@10.8.40.46>
2018-09-06 18:17:58 +0800

    2018/9/6 delete error

--- src/main/java/jdk8/sharing/chapt13/TestListInteger.java
+++ src/main/java/jdk8/sharing/chapt13/TestListInteger.java
@@ -1,8 +1,14 @@
 package jdk8.sharing.chapt13;
 
+import java.util.Arrays;
+
 public class TestListInteger {
 	
 	private static int[] arrs = {1, 4, 9};
+	
+	private static void getSubset() {
+//		Arrays.stream(arrs).map(mapper)
+	}
 
 	public static void main(String[] args) {
 
